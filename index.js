07c47040a301cb1a7a092ae6660fdeee8ad094ed
Your Name <you@example.com>
2020-06-08 05:48:31 -0300

    agregados de elementos html5 y cambios estructurales

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-function toggleMenu() {
-    var x = document.getElementById("main-1");
+function toggleMainMenu() {
+    var x = document.getElementById("main-menu");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function toggleUserMenu() {
+    var x = document.getElementById("user-menu");
     if (x.style.display === "none") {
       x.style.display = "flex";
     } else {
